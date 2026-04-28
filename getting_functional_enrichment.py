049f155faa1a5e774eea9f71ed4a9c6d302b965a
Gonçalo Maria <goncalomaria2001@gmail.com>
2026-04-28 14:38:12 +0100

    Update getting_functional_enrichment.py

--- getting_functional_enrichment.py
+++ getting_functional_enrichment.py
@@ -33,11 +33,38 @@ def get_loc_intersections(paths_to_venn_txts, strains):
     return loc_intersections
 
 def convert_IDs_to_string(loc_slices):
+    ## snippet adapted from string api examples
 
-def get_string_functional_annotation(string_IDs):
+    string_api_url = "https://version-12-0.string-db.org/api"
+    output_format = "tsv-no-header"
+    method = "get_string_ids"
+
+    for loc in loc_slices:
+        for slice in loc_slices[loc]:
+            time.sleep(2)
+            params = {
+
+                "identifiers": "\r".join(loc_slices[loc][slice]['IDs']),  # your protein list
+                "species": NCBI_taxon,  # NCBI/STRING taxon identifier
+                "echo_query": 1,  # see your input identifiers in the output
+                "caller_identity": "www.awesome_app.org"  # your app name
+
+            }
+
+            request_url = "/".join([string_api_url, output_format, method])
+            results = requests.post(request_url, data=params)
+            with open(os.path.join(general_path, 'ID_tables', f'{loc} - {slice}.tsv'), 'w') as f:
+                f.write(results.text)
+            print(f'{loc} - {slice} done')
+
+
+
+
+## def get_string_functional_annotation(string_IDs):
 
 general_path = r'C:\Users\gomam\Estágio ITQB\Growth curve MIC PA 11k 13k\PsortB\Results\Loc_comparisons'
 strains = ['11k', '13k', 's14', 'RP']
+NCBI_taxon = 1282
 
 paths_to_venn_txts = get_path_to_venn_txts(general_path)
 loc_slices = get_loc_intersections(paths_to_venn_txts, strains)
